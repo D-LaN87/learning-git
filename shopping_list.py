82b2d4290e4033c54f7981abe992aaad9bebe183
Your Name <adamski_leszek@wp.pl>
2025-04-23 21:11:31 +0200

    Add number_of_items_counter

--- shopping_list.py
+++ shopping_list.py
@@ -4,3 +4,12 @@ shopping_list = {
 }
 for k, v in shopping_list.items():
     print("Wchodzę do " + str(k).title() + ", kupuję tu następujące rzeczy: " + str(v).title())
+
+items = 0
+
+for item in shopping_list.keys():
+    number_of_items = shopping_list[item]
+    for i in number_of_items:
+        items = items + 1
+        
+print("W sumię kupuję " + str(items) + " produktów. \n")
