8c9e9d880d8a78c28617b9edb131fe3549d142be
Your Name <adamski_leszek@wp.pl>
2025-04-23 22:11:49 +0200

    Add more products to shops

--- shopping_list.py
+++ shopping_list.py
@@ -1,6 +1,6 @@
 shopping_list = {
-    "piekarnia" : ["chleb", "pączek", "bułki"],
-    "warzywniak" : ["marchew", "seler", "rukola"],
+    "piekarnia" : ["chleb", "pączek", "bułki", "drożdżówka"],
+    "warzywniak" : ["marchew", "seler", "rukola", "pomidor"],
 }
 for k, v in shopping_list.items():
     print("Wchodzę do " + str(k).title() + ", kupuję tu następujące rzeczy: " + str(v).title())
