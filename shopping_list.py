e3ebb14bbe0ef7606684c146c5d97eac954c258c
Your Name <adamski_leszek@wp.pl>
2025-04-23 21:06:11 +0200

    Add iteration on values

--- shopping_list.py
+++ shopping_list.py
@@ -2,3 +2,5 @@ shopping_list = {
     "piekarnia" : ["chleb", "pączek", "bułki"],
     "warzywniak" : ["marchew", "seler", "rukola"],
 }
+for k, v in shopping_list.items():
+    print("Wchodzę do " + str(k).title() + ", kupuję tu następujące rzeczy: " + str(v).title())
